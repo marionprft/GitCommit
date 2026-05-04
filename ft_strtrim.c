6f6d0e2fff843b240df5ddd4f97de0f38a7edf6d
mapointi mapointi <marion.pointier-fourcart@learner.42.tech>
2026-05-04 23:50:30 +0200

    maj trim Estelle

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mapointi <mapointi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 18:21:28 by mapointi          #+#    #+#             */
-/*   Updated: 2026/05/04 23:00:33 by mapointi         ###   ########.fr       */
+/*   Updated: 2026/05/04 23:49:03 by mapointi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,28 +47,44 @@ char    *ft_strtrim(char const *s1, char const *set)
     i = ft_strlen(s2) - 1;
     j = 0;
     while (i > 0)
-    { 
+    {  
+        if (set[j] == s2[i])
+            i--;
         if (set[j] != s2[i])
         {
             j = 0;
             while (set[j] && set[j] != s2[i])
-            j++;
-        }
-        if (set[j] == s2[i])
-            i--;
+            {
+                j++;
+                printf("%ld\n", i);
+            }
+            
+            //printf("%c\n", set[i]);
+        } 
+        break;
     }
+   
     s3 = (char  *)malloc(sizeof(char) * (i + 1));
+    if (!s3)
+        return (NULL);
+    j = 0;
+    while (j <= i)
+    {
+        s3[j] = s2[j];
+        j++;
+    }
+    s3[j] = 0;
     return (s3);
 }
 
 int main(void)
 {
     char    *s1 = "?55]ah bah dakkeur///?55]";
-    char    *s2 = "?55]ah bah dakkeur///?55]boop";
-    char    *s3 = "?55]ah bah dakkeur///?55";
+    // char    *s2 = "?55]ah bah dakkeur///?55]boop";
+    // char    *s3 = "?55]ah bah dakkeur///?55";
     char    *set = "?55]";
 
     printf("%s\n", ft_strtrim(s1, set));
-    printf("%s\n", ft_strtrim(s2, set));
-    printf("%s\n", ft_strtrim(s3, set));
+    // printf("%s\n", ft_strtrim(s2, set));
+    // printf("%s\n", ft_strtrim(s3, set));
 }
