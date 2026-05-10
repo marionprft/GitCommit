6e780e8810f0a6513d8ccda37498c42c27990601
mapointi mapointi <marion.pointier-fourcart@learner.42.tech>
2026-05-10 21:19:39 +0200

    liste chainees

---
+++ ft_lstnew.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapointi <mapointi@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/10 16:36:25 by mapointi          #+#    #+#             */
+/*   Updated: 2026/05/10 20:58:29 by mapointi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+t_list  *ft_lstnew(void *content)
+{
+    t_list  *newnode;
+
+    newnode = malloc(sizeof(t_list));
+    if (!newnode)
+        return (NULL);
+    newnode->content = content;
+    newnode->next = NULL;
+    return (newnode);
+}
+
+int     main(void)
+{
+    t_list *a;
+    char    *s = "42";
+    a = ft_lstnew((char *)s);
+    printf("Content : %s\n", (char  *)a->content);
+    printf("Adresse prochain node : %p\n", a->next);
+    return (0);
+}
