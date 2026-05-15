7bcad5cb58b18100b90647722cac862e56e158c3
Marion Pointier-Fourcart <mpf@minitel.local>
2026-05-15 14:34:24 +0200

    addback fini

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mpf <mpf@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 19:10:48 by mapointi          #+#    #+#             */
-/*   Updated: 2026/05/15 00:51:01 by mpf              ###   ########.fr       */
+/*   Updated: 2026/05/15 14:33:47 by mpf              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,22 @@ int	main(void)
 	t_list *lst = NULL;
 	t_list *node1 = malloc(sizeof(t_list));
 	t_list *node2 = malloc(sizeof(t_list));
-
+	
 	node1->content = "Hello ";
 	node2->content = "World ";
 
 	t_list *new = malloc(sizeof(t_list));
 	new->content = "!";
+	new->next = NULL;
 
 	lst = node1;
 	node1->next = node2;
 
 	ft_lstadd_back(&lst, new);
-	printf("%s", (char *)lst->content);
+	t_list *mobile = lst;
+	while(mobile != NULL)
+	{
+		printf("%s", (char *)mobile->content);
+		mobile = mobile->next;
+	}
 }
